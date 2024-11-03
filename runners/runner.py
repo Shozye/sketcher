9d5506518f6b856ef9aa3d8beb87b8fbd17fd50e
Mateusz <sshozye@gmail.com>
2024-11-03 08:12:22 +0100

    created a runner to run sketcher in a scripted way without using bash (not a fan)

--- runners/runner.py
+++ runners/runner.py
@@ -3,10 +3,13 @@ from dataclasses import dataclass
 import os
 import subprocess
 
+class Constants:
+    DATA_DIRNAME = "data"
+    PATH_TO_BINARY=os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "build", "bin", "sketcher")
+    OUTPUT_DIR_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), DATA_DIRNAME)
 
 @dataclass
 class CommandLineArgs:
-    path_to_binary: str
     command: str
     sketch_name: str
     weight_dist_seed: int = 0  # Default value: generates random seed if 0
@@ -19,11 +22,11 @@ class CommandLineArgs:
     sketchSize: int = 400  # Default sketch size
     sketchStartSeed: int = 42  # Default starting seed for sketches
     qsketchStart: float = 0.25  # Optional QSketch specific
-    qsketchEnd: float = 1  # Optional QSketch specific
+    qsketchEnd: float = 0.75  # Optional QSketch specific
     qsketchAmountPoints: int = 5000  # Optional QSketch specific
 
     def to_command(self) -> list[str]:
-        command = (f"{self.path_to_binary} {self.command} -name {self.sketch_name}"
+        command = (f"{Constants.PATH_TO_BINARY} {self.command} -name {self.sketch_name}"
                    f" -weightDist {self.weightDistType} {self.weightDistArg1} {self.weightDistArg2}"
                    f" -sketches {self.amount_sketches} -elems {self.amount_elements}"
                    f" -itemDist {self.itemDist} -weightSeed {self.weight_dist_seed}"
@@ -33,17 +36,31 @@ class CommandLineArgs:
 
         return command.split(" ")
     
+def run_sketcher(args: CommandLineArgs, filename: str):
+
+    with open(os.path.join(Constants.OUTPUT_DIR_PATH, filename), 'w+') as file:
+        result = subprocess.run(args.to_command(), stdout=file)
+        print(result)
+    
 def main():
-    DATA_DIRNAME = "data"
-    os.makedirs(DATA_DIRNAME, exist_ok=True)
+    os.makedirs(Constants.DATA_DIRNAME, exist_ok=True)
 
-    path_to_binary=os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "build", "bin", "sketcher")
-    output_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), DATA_DIRNAME, "output.txt")
-    cmd = CommandLineArgs(path_to_binary, "test", "qsketch")
+    args = []
+    for sketch_name in ["qsketch", "fastexpsketch"]:
+        for amount_elems in [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000]:
+            args.append((sketch_name, amount_elems))
 
-    with open(output_path, 'w+') as file:
-        result = subprocess.run(cmd.to_command(), stdout=file)
-    print(result)
+    for i, (sketch_name, amount_elems) in enumerate(args):
+
+        cmd = CommandLineArgs("test", sketch_name=sketch_name, amount_elements=amount_elems, amount_sketches=1)
+        output_filename = f"{sketch_name}_uniform11_e{amount_elems}.txt"
+        
+        run_sketcher(cmd, output_filename)
+
+
+    run_sketcher(cmd, "qsketch1.txt")
+
+    
 
 if __name__ == "__main__":
     main()
